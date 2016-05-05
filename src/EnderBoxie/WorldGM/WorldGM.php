0f5b608420bb5259ef585d524adbb843990a5c4c
Reziq Vavai <kakakvavai66@gmail.com>
2016-05-05 20:24:11 +0700

    Update WorldGM.php

--- src/EnderBoxie/WorldGM/WorldGM.php
+++ src/EnderBoxie/WorldGM/WorldGM.php
@@ -1,1 +1,13 @@
+<?php
 namespace EnderBoxie\WorldGM;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
+
+class WorldGM extends PluginBase{
+    
+    public $cfg;
+    
+    public function onEnable(){}
+}
