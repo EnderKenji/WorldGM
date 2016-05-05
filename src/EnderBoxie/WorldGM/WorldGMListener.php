06232119841a7efc6836acf661aef052d20ba4d7
Reziq Vavai <kakakvavai66@gmail.com>
2016-05-05 20:41:43 +0700

    Update WorldGMListener.php

--- src/EnderBoxie/WorldGM/WorldGMListener.php
+++ src/EnderBoxie/WorldGM/WorldGMListener.php
@@ -1,12 +1,23 @@
 <?php
 namespace EnderBoxie\WorldGM;
 
+use pocketmine\Player;
 use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\level\Level;
 
 class WorldGMListener implements Listener{
 
     public function __construct(WorldGM $plugin){
         $this->plugin = $plugin;
+    }
+
+    public function onLevelChange(EntityLevelChangeEvent $event){
+        $p = $event->getEntity();
+        $target = $event->getTarget();
+        if($p instanceof Player){
+            // To-Do
+        }
     }
 
 }
