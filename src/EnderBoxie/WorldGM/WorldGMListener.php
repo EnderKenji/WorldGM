e1cb4491de9aa63148ba59e2a81763dd8caff4a4
Reziq Vavai <kakakvavai66@gmail.com>
2016-05-05 20:31:19 +0700

    Update WorldGMListener.php

--- src/EnderBoxie/WorldGM/WorldGMListener.php
+++ src/EnderBoxie/WorldGM/WorldGMListener.php
@@ -1,1 +1,12 @@
+<?php
 namespace EnderBoxie\WorldGM;
+
+use pocketmine\event\Listener;
+
+class WorldGMListener implements Listener{
+
+    public function __construct(WorldGM $plugin){
+        $this->plugin = $plugin;
+    }
+
+}
